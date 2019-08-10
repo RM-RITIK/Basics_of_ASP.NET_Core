3aba8c812db0fd5fc96e1d0850a9112ac757fbe1
RM-RITIK <mehtaritik11@gmail.com>
2019-08-10 21:42:47 +0530

    first commit

---
+++ Empty_template/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Empty_template
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+
+
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else if(env.IsProduction() || env.IsStaging())
+            {
+                app.UseExceptionHandler("Error");
+            }
+
+            /*The below codes does the same function*/
+
+            /*DefaultFilesOptions defaultFiles = new DefaultFilesOptions();
+            defaultFiles.DefaultFileNames.Clear();
+            defaultFiles.DefaultFileNames.Add("foo.html");
+            app.UseDefaultFiles(defaultFiles);
+            app.UseStaticFiles();*/
+
+            FileServerOptions fileServerOptions = new FileServerOptions();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
+            app.UseFileServer(fileServerOptions);
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW1: Incoming Request");
+                await next();
+                logger.LogInformation("MW1: Outgoing Request");
+            });
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW2: Incoming Request");
+                await next();
+                logger.LogInformation("MW2: Outgoing Request");
+            });
+
+            app.Run(async (context) =>
+            {
+                throw new Exception("Some Error processing the request");
+                await context.Response.WriteAsync("Hello World");
+            });
+        }
+    }
+}
